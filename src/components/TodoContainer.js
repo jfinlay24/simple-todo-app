be6212dde07b23f0dbcb8424b79eb5ae721f07fa
Jonathan Finlay <jfinlay24@yahoo.ie>
2020-06-24 19:55:57 +0100

    Finished Part 1.

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,10 +1,38 @@
 import React from "react"
+import Header from "./Header"
+import TodosList from "./TodosList";
+
 class TodoContainer extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false
+      }
+    ]
+   };
+  
   render() {
     return (
       <div>
-        <h1>Hello from Create React App</h1>
-        <p>I am in a React Component!</p>
+        <Header />
+        <TodosList todos={this.state.todos} />
+        {/*<h1>Simple ToDo App</h1>
+        <p>Please add to-dos item(s) through the input field</p>
+        {this.state.todos.map(todo => (
+          <li>{todo.title}</li>
+        ))}*/}
       </div>
     )
   }
